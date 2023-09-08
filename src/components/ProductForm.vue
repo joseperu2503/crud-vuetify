cc520be4891acb0b2a48a613154d6e292dd409a3
Joseperez <51133263+joseperu2503@users.noreply.github.com>
2023-09-07 19:32:26 -0500

    Update ProductForm.vue

--- src/components/ProductForm.vue
+++ src/components/ProductForm.vue
@@ -1,21 +1,56 @@
 <template>
-  <v-dialog :modelValue="showModal" width="95%" max-width="500px"
+  <v-dialog :modelValue="showModal" width="95%" max-width="800px"
     @update:modelValue="($event) => emit('update:showModal', $event)">
     <v-card :title="title" :loading="loading">
       <v-card-text>
         <v-form @keydown.enter="submit" class="mt-6">
-          <v-text-field label="Name*" v-model="form.name" :error-messages="errors.name?.[0]" class="mt-4"
-            color="primary" />
-          <v-text-field label="Price*" type="number" v-model="form.price" :error-messages="errors.price?.[0]" prefix="$"
-            class="mt-4" color="primary" />
-          <v-text-field label="Stock*" type="number" v-model="form.stock" :error-messages="errors.stock?.[0]" class="mt-4"
-            color="primary" />
-          <v-file-input accept="image/png, image/jpeg" prepend-icon="mdi-camera" label="Images" v-model="images"
-            :error-messages="errors.images?.[0]" multiple class="mt-4" />
+          <v-row>
+            <v-col cols="12" md="7">
+              <v-text-field label="Name*" v-model="form.name" :error-messages="errors.name?.[0]" class="mt-4"
+                color="primary" />
+              <v-textarea variant="filled" label="Description" auto-grow
+                model-value="The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."></v-textarea>
+              <v-autocomplete label="Brand"
+                :items="['Nike', 'Adidas', 'Puma', 'Xiaomi', 'Apple', 'Samsung']"></v-autocomplete>
+              <v-text-field label="Price*" type="number" v-model="form.price" :error-messages="errors.price?.[0]"
+                prefix="$" class="mt-4" color="primary" />
+              <v-text-field label="Stock*" type="number" v-model="form.stock" :error-messages="errors.stock?.[0]"
+                class="mt-4" color="primary" />
+              <v-autocomplete label="Category"
+                :items="['Electronics', 'Clothing and Fashion', 'Home and Garden', 'Health and Beauty', 'Toys and Entertainment', 'Pets', 'Jewelry and Watches']"></v-autocomplete>
+              <v-btn-toggle multiple divided variant="outlined" color="primary">
+                <v-btn value="s">Hombre</v-btn>
+                <v-btn value="m">Mujer</v-btn>
+                <v-btn value="l">Niño</v-btn>
+                <v-btn value="xl">Niña</v-btn>
+              </v-btn-toggle>
+              <v-btn-toggle multiple divided variant="outlined" color="primary" class="mt-4">
+                <v-btn value="s">S</v-btn>
+                <v-btn value="m">M</v-btn>
+                <v-btn value="l">L</v-btn>
+                <v-btn value="xl">XL</v-btn>
+              </v-btn-toggle>
+            </v-col>
+            <v-col cols="12" md="5">
+
+
+              <v-color-picker :modes="['hex']" width="100%"></v-color-picker>
+              <v-btn class="mt-4 w-100" prepend-icon="mdi-plus">
+                Add color
+              </v-btn>
+              <v-file-input accept="image/png, image/jpeg" prepend-icon="mdi-camera" label="Images" v-model="images"
+                :error-messages="errors.images?.[0]" multiple class="d-none" ref="imageInput" />
+              <v-btn prepend-icon="mdi-camera" class="mt-4 w-100" @click="clickAddImage">
+                Subir imagen
+              </v-btn>
+              <v-carousel hide-delimiters v-if="form.images.length > 0" height="300" class="mt-4">
+                <v-carousel-item v-for="(image, index) in form.images" :key="index" :src="image"
+                  contain></v-carousel-item>
+              </v-carousel>
+              <v-switch  hide-details  label="Publico" color="primary"></v-switch>
+            </v-col>
+          </v-row>
         </v-form>
-        <v-carousel hide-delimiters v-if="form.images.length > 0" height="300">
-          <v-carousel-item v-for="(image, index) in form.images" :key="index" :src="image" contain></v-carousel-item>
-        </v-carousel>
       </v-card-text>
       <v-card-actions>
         <v-spacer></v-spacer>
@@ -137,4 +172,10 @@ const submit = async () => {
   submitLoading.value = false
 }
 
+const imageInput = ref<any>(null)
+
+const clickAddImage = () => {
+  console.log('click')
+  console.log(imageInput.value?.click())
+}
 </script>
